600e23a723ae518a8ae1e61fff4f6efe41fedc8a
justmfundo-github <justmfundo@gmail.com>
2023-11-13 10:21:38 +0200

    HomePage completed TeamTile on HomePage completed

--- ipl-dashboard/src/frontend/src/pages/HomePage.js
+++ ipl-dashboard/src/frontend/src/pages/HomePage.js
@@ -1,10 +1,8 @@
 import "./HomePage.scss";
 import { React, useEffect, useState } from "react";
-import { useParams, userParams } from "react-router-dom";
-import { MatchDetailCard } from "../components/MatchDetailCard";
-import { MatchSmallCard } from "../components/MatchSmallCard";
-import { PieChart } from "react-minimal-pie-chart";
+import { TeamTile } from "../components/TeamTile";
 import { Link } from "react-router-dom";
+import { useParams, userParams } from "react-router-dom";
 
 export const HomePage = () => {
   // declaring a state called team and a function called setTeam to populate that state
@@ -13,15 +11,27 @@ export const HomePage = () => {
   useEffect(() => {
     //useEffect cannot be asynchronous so we create another function within the useEffect function that is asynchronous
     // fetching teams from the api endpoint
-    const fetchTeams = async () => {
+    const fetchAllTeams = async () => {
       const response = await fetch(`http://localhost:8080/teams`);
       const data = await response.json();
       console.log(data);
 
       setTeams(data);
     };
-    fetchTeams();
-  });
+    fetchAllTeams();
+  }, []);
 
-  return <div className="HomePage">The life and times of Sean Carter</div>;
+  return (
+    <div className="HomePage">
+      <div className="header-section">
+        <h1 className="app-name">IPL DashBoard Curtesy of JavaBrains Tutorial</h1>
+      </div>
+
+      <div className="team-grid">
+        {teams.map((team) => (
+          <TeamTile teamName={team.teamName} />
+        ))}
+      </div>
+    </div>
+  );
 };
